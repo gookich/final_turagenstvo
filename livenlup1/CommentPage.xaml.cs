803d642a51700d3e05f03aa2270e78e1165c5ef1
Gookich <Gookich@DESKTOP-D0GP6EV>
2022-11-01 23:56:32 +0800

    Добавьте файлы проекта.

---
+++ livenlup1/CommentPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace livenlup1
+{
+    /// <summary>
+    /// Логика взаимодействия для CommentPage.xaml
+    /// </summary>
+    public partial class CommentPage : Page
+    {
+        private hotel _currentComment = new hotel();
+        public CommentPage(hotel selectedHotel)
+        {
+            InitializeComponent();
+            DataContext = _currentComment;
+            //ComboHotel.ItemsSource = ToursBaseEntities.GetContext().hotels.ToList();
+        }
+
+        private void ButtonSend_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Комментарий сохранен");
+        }
+    }
+}
